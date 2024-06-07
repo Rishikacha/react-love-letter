8c0073a5e051ae8cb23db25a0387bf3a9ad9e411
Rishikacha <rishikadulla6@gmail.com>
2024-06-07 05:25:21 -0700

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -35,9 +35,7 @@ const LoveLetter = () => {
       <div className="flap"></div>
       <div className="body"></div>
       <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
-        Hiiiii maratledhu <br />i loveeee youuuu :)<br />
-        always yours,<br />
-        @niihaaarrrr
+        Happy Birthday dear mine ❤️ee roju special day to me.. nannu yepudu support chesthaav.. life long yevaru vuntaro ledho thelidhu kaani nuvvu matram naku kavalii..na midha kante ni midhe chala nammakam nakuu🥹.. yepudu manam support cheskuntane vuntam..even in fights 😁...manam idharam okate bava 🥺I don't want any secrets or misunderstanding between us .okkosari nene picha thepisthaanu 😅 kopam thepisthuna vadhileyav kadha ..?🥺..yedho kopam lo antaanu kaanii okka roju kaadhu kadha oka ganta kuda vundalenu matladakundaa..kottukonoo ,thittukuno matladukovali😁..chala misss avuthunaa bava ninnu 🥺yenni days ayindhi thelusaa kalisii 🥲..okati chepthunanu vinu bava niku yepudu alone Ane feel ranivanu.yepudu yem vachinaa nenu okadhanini vunaanu ani gurthupetuko..yevadini yedhi Adukokuu  nikavasaram ledhu.mana idharilo yevariki ye kastam vachinaa solve cheskundhamm..nakem vaddhu kani okati aduguthaa bava nakepudu alone feel thepinchaku ee holidays ichina starting lo anukuntaa chala ignore chesaaav nannuu worst situation ever malli naku aa situation assalu vaddhuu pleaseee😖..kopam vasthe oka sorry 😁prema vasthe oka I love you 🫣 chepthe set ayipothanu nannu handle cheyadam chala easy babu🤭nenu thikkagaa vaagaaanh antee nuvvu premaga matladi kopam thaginchali ani ardham 😁..ninnu control cheyadam chala easy lee 😉ni gurinchi naku thelikundaa vundadhaa 😁..once again Happy Birthday Bavaaaaaa and I love you more than myself 🥹💋😘 asalee nakuu bujjji pilodivii mallii ipude putaav chala jagratha ga chuskovalamma🤭
       </div>
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
     </div>
